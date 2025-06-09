33da48e3e6b2b3a611445aea98dbed7bb25c29fa
DISHANT <bheredishant3@gmail.com>
2025-06-09 20:59:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,34 +3,33 @@ let show = document.getElementById("show");
 let search = document.getElementById("search");
 let cityVal = document.getElementById("city");
 
-//Make sure you have your own key.
-let key = "2f745fa85d563da5adb87b6cd4b81caf";
+// Your new WeatherAPI key
+let key = "6f39aba5e5f14e70a5e151512250906";
 
 let getWeather = () => {
   let cityValue = cityVal.value;
   if (cityValue.length == 0) {
     show.innerHTML = `<h3 class="error">Please enter a city name</h3>`;
-  }
-  else {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+  } else {
+    let url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${cityValue}&aqi=no`;
     cityVal.value = "";
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => {
         show.innerHTML = `
-        <h2>${data.name}, ${data.sys.country}</h2>
-        <h4 class="weather">${data.weather[0].main}</h4>
-        <h4 class="desc">${data.weather[0].description}</h4>
-        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-        <h1>${data.main.temp} &#176;</h1>
+        <h2>${data.location.name}, ${data.location.country}</h2>
+        <h4 class="weather">${data.current.condition.text}</h4>
+       
+        <img src="https:${data.current.condition.icon}">
+        <h1>${data.current.temp_c} &#176;</h1>
         <div class="temp_container">
          <div>
-            <h4 class="title">min</h4>
-            <h4 class="temp">${data.main.temp_min}&#176;</h4>
+            <h4 class="title">Feels</h4>
+            <h4 class="temp">${data.current.feelslike_c}&#176;</h4>
          </div>
          <div>
-            <h4 class="title">max</h4>
-            <h4 class="temp">${data.main.temp_max}&#176;</h4>
+            <h4 class="title">Humidity</h4>
+            <h4 class="temp">${data.current.humidity}%</h4>
          </div>   
         </div>
         `;
@@ -40,8 +39,10 @@ let getWeather = () => {
       });
   }
 };
+
 search.addEventListener("click", getWeather);
 window.addEventListener("load", getWeather);
+
 // -------------------- RANDOM BACKGROUND ON LOAD --------------------
 const backgrounds = [
   "images/background1.jpg",
@@ -59,3 +60,4 @@ document.body.style.backgroundImage = `url('${selected}')`;
 document.body.style.backgroundSize = "cover";
 document.body.style.backgroundPosition = "center";
 document.body.style.backgroundRepeat = "no-repeat";
+
